78b0fd6a7f161adfd15c51e1a79275192f3d16bb
Bruno Cruz <bru.cruz@gmail.com>
2023-05-23 18:21:22 -0300

    feat: implemented users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # create a route for product search where there will be a form get the url to be scraped
+  get '/product/search', to: 'products#search'
+  # create a route for product result where the scraped data will be displayed
+  get '/product/result', to: 'products#result'
+
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
