5c2f73256ae4dbf0e83938b024435f656887a638
Bruno Cruz <bru.cruz@gmail.com>
2023-06-02 11:50:59 -0300

    changed deploy url for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 
 ## Defaults:
 # set :scm,           :git
-# set :branch,        :main
+set :branch,        :main
 # set :format,        :pretty
 # set :log_level,     :debug
 # set :keep_releases, 5
