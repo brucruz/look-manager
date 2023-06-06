b342baa947d2b2cc49d9497b420a6c18949484a4
Bruno Cruz <bru.cruz@gmail.com>
2023-06-05 22:37:08 -0300

    redo the capybara selenium driver config based on the buildpack docs

--- app/services/oqvestir_scraper.rb
+++ app/services/oqvestir_scraper.rb
@@ -70,9 +70,18 @@ class OqvestirScraper
     Capybara.app_host = 'https://www.oqvestir.com.br'
     Capybara.ignore_hidden_elements = false
 
+    chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+
+    chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
     Capybara.register_driver :chrome do |app|
-      Capybara::Selenium::Driver.new app, browser: :chrome,
-        options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu disable-dev-shm-usage])
+      Capybara::Selenium::Driver.new(
+        app,
+        browser: :chrome,
+        desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+      )
     end
+
+    Capybara.javascript_driver = :chrome
   end
 end
