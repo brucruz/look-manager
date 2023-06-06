fbb3ada21c7126febeb9edd5f12a07dbeae94c3e
Bruno Cruz <bru.cruz@gmail.com>
2023-06-06 00:03:06 -0300

    added new options to capybara/selenium

--- app/services/oqvestir_scraper.rb
+++ app/services/oqvestir_scraper.rb
@@ -81,6 +81,9 @@ class OqvestirScraper
     options.add_argument("--start-maximized")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--disable-notifications")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-extensions")
 
     chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
 
