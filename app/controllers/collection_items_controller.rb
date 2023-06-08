dd61ab330dd5cfa8c2c5a2d23d3b7812336639ec
Bruno Cruz <bru.cruz@gmail.com>
2023-06-08 17:34:20 -0300

    feat: permit stylist to add an item to her collection even if does not give any classification

--- app/controllers/collection_items_controller.rb
+++ app/controllers/collection_items_controller.rb
@@ -1,7 +1,11 @@
 class CollectionItemsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
-    @collection_item = @product.collection_items.create({ user_id: current_user.id, **collection_item_params })
+    
+    create_params = { user_id: current_user.id }.merge(collection_item_params&.to_h || {})
+
+    @collection_item = @product.collection_items.create(create_params)
+    
     redirect_to product_path(@product)
   end
   
@@ -15,6 +19,6 @@ class CollectionItemsController < ApplicationController
         contrast: [],
         style: [],
         body_type: []
-      )
+      ) if params[:collection_item].present?
     end
 end
