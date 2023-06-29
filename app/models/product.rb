42a8d61c2b8d2f60a7a10cc5c98cedb004897722
Bruno Cruz <bru.cruz@gmail.com>
2023-06-28 22:06:09 -0300

    feat: configure search method for Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,23 @@
 class Product < ApplicationRecord
   has_many :collection_items, dependent: :destroy
   has_many :users, through: :collection_items
+
+  include PgSearch::Model
+  pg_search_scope :search_product,
+                against: {
+                  name: 'A',
+                  brand: 'A',
+                  store: 'A',
+                  description: 'B',
+                },
+                using: {
+                  tsearch: {
+                    prefix: true,
+                    dictionary: 'portuguese'
+                  },
+                  trigram: {
+                    word_similarity: true
+                  }
+                },
+                ignoring: :accents
 end
