93c6d140c8e6485e5cbb9a399c98df37e651ed28
Bruno Cruz <bru.cruz@gmail.com>
2023-07-12 22:40:51 -0300

    feat: config I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,26 @@ class ApplicationController < ActionController::Base
 
       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password)}
     end
+
+  around_action :switch_locale
+
+  # This method is used to change the locale of the application using paramaters
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  # TODO: future implementation, activate this method to change the locale of the application using the browser language
+  # def switch_locale(&action)
+  #   logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+  #   locale = extract_locale_from_accept_language_header
+  #   logger.debug "* Locale set to '#{locale}'"
+  #   I18n.with_locale(locale, &action)
+  # end
+  
+  # private
+  #   def extract_locale_from_accept_language_header
+  #     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  #   end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,17 @@ module LookManager
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Configuration for I19n load path
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    # Configuration for default locale
+    config.i18n.default_locale = "pt-BR"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "America/Sao_Paulo"
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.active_record.schema_format = :sql
