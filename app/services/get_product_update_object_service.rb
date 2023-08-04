21b2069551953824fcdb453691055c70bbf8c16b
Bruno Cruz <bru.cruz@gmail.com>
2023-08-04 12:12:45 -0300

    chore: part 1

--- app/services/get_product_update_object_service.rb
+++ app/services/get_product_update_object_service.rb
@@ -1,90 +1,144 @@
 ## it receives a product object and a hash with the new attributes, then it updates the product and returns it
 class GetProductUpdateObjectService
-  def initialize(product, new_attributes)
-    @old_attributes = get_params(product)
-    @new_attributes = get_params(new_attributes)
+  def initialize(initial_product_object, new_object_attributes)
+    @old_object_attributes = get_params(initial_product_object)
+    @new_object_attributes = get_params(new_object_attributes)
+    @product_id = initial_product_object["product"][:id]
   end
 
   def call
     product_to_update = {}
-    product_to_update = { **@new_attributes }
+    variant_to_update = {}
 
-    # if new name, sku, brand, description, images and sizes attributes are missing use the old product attributes
-    if @new_attributes[:name].nil?
-      product_to_update[:name] = @old_attributes[:name]
+    product_to_update = @new_object_attributes["product"]
+    variants_to_update = @new_object_attributes["variants"]
+
+    # treating product attributes
+    if product_to_update["sku"].nil?
+      product_to_update["sku"] = @old_attributes["product"]["sku"]
     end
-    if @new_attributes[:sku].nil?
-      product_to_update[:sku] = @old_attributes[:sku]
-    end
-    if @new_attributes[:brand].nil?
-      product_to_update[:brand] = @old_attributes[:brand]
-    end
-    if @new_attributes[:description].nil?
-      product_to_update[:description] = @old_attributes[:description]
-    end
-    if @new_attributes[:images].nil? || @new_attributes[:images].empty?
-      product_to_update[:images] = @old_attributes[:images]
-    end
-    if @new_attributes[:sizes].nil? || @new_attributes[:sizes].empty?
-      product_to_update[:sizes] = @old_attributes[:sizes]
+    if product_to_update["brand"].nil?
+      product_to_update["brand"] = @old_attributes["product"]["brand"]
     end
 
-    # if both old_price and price attributes are missing use the old product attributes
-    if @new_attributes[:old_price].nil? && @new_attributes[:price].nil?
-      product_to_update[:old_price] = @old_attributes[:old_price]
-      product_to_update[:price] = @old_attributes[:price]
-    end
+    # treating variant attributes
+    variants_to_update.map.with_index do |variant_to_update, index|
+      new_variant_attributes = @new_object_attributes["variants"][index]
+      old_variant_attributes = @old_object_attributes["variants"][index]
 
-    # if price is missing, use the previous old_price and price attributes
-    if @new_attributes[:price].nil?
-      product_to_update[:old_price] = @old_attributes[:old_price]
-      product_to_update[:price] = @old_attributes[:price]
-    end
-
-    
-    # if installment quantity or value is missing...
-    if [@new_attributes[:installment_quantity], @new_attributes[:installment_value]].any?(&:nil?)
-      # check first if both installment_quantity and installment_value attributes are missing...
-      if @new_attributes[:installment_quantity].nil? && @new_attributes[:installment_value].nil?
-        ## check if price is also missing...
-        if @new_attributes[:price].nil?
-          ## if it is missing, then return old attributes
-          product_to_update[:installment_quantity] = @old_attributes[:installment_quantity]
-          product_to_update[:installment_value] = @old_attributes[:installment_value]
-        else
-          ## return new attributes
-          product_to_update[:installment_quantity] = @new_attributes[:installment_quantity]
-          product_to_update[:installment_value] = @new_attributes[:installment_value]
-        end
-      else
-        # if only one of them is nil, then use the old attributes
-        product_to_update[:installment_quantity] = @old_attributes[:installment_quantity]
-        product_to_update[:installment_value] = @old_attributes[:installment_value]
+      # if new name, sku, brand, description, images and sizes attributes are missing use the old product attributes
+      if new_variant_attributes["title"].nil?
+        variant_to_update["title"] = old_variant_attributes["title"]
       end
+      if new_variant_attributes["full_name"].nil?
+        variant_to_update["full_name"] = old_variant_attributes["full_name"]
+      end
+      if new_variant_attributes["sku"].nil?
+        variant_to_update["sku"] = old_variant_attributes["sku"]
+      end
+      if new_variant_attributes["description"].nil?
+        variant_to_update["description"] = old_variant_attributes["description"]
+      end
+      if new_variant_attributes["images"].nil? || new_variant_attributes["images"].empty?
+        variant_to_update["images"] = old_variant_attributes["images"]
+      end
+      if new_variant_attributes["sizes"].nil? || new_variant_attributes["sizes"].empty?
+        variant_to_update["sizes"] = old_variant_attributes["sizes"]
+      end
+  
+      # if both old_price and price attributes are missing use the old product attributes
+      if new_variant_attributes["old_price"].nil? && new_variant_attributes["price"].nil?
+        variant_to_update["old_price"] = old_variant_attributes["old_price"]
+        variant_to_update["price"] = old_variant_attributes["price"]
+      end
+  
+      # if price is missing, use the previous old_price and price attributes
+      if new_variant_attributes["price"].nil?
+        variant_to_update["old_price"] = old_variant_attributes["old_price"]
+        variant_to_update["price"] = old_variant_attributes["price"]
+      end
+  
+      
+      # if installment quantity or value is missing...
+      if [new_variant_attributes["installment_quantity"], new_variant_attributes["installment_value"]].any?(&:nil?)
+        # check first if both installment_quantity and installment_value attributes are missing...
+        if new_variant_attributes["installment_quantity"].nil? && new_variant_attributes["installment_value"].nil?
+          ## check if price is also missing...
+          if new_variant_attributes["price"].nil?
+            ## if it is missing, then return old attributes
+            variant_to_update["installment_quantity"] = old_variant_attributes["installment_quantity"]
+            variant_to_update["installment_value"] = old_variant_attributes["installment_value"]
+          else
+            ## return new attributes
+            variant_to_update["installment_quantity"] = new_variant_attributes["installment_quantity"]
+            variant_to_update["installment_value"] = @new_attributes["installment_value"]
+          end
+        else
+          # if only one of them is nil, then use the old attributes
+          variant_to_update["installment_quantity"] = old_variant_attributes["installment_quantity"]
+          variant_to_update["installment_value"] = old_variant_attributes["installment_value"]
+        end
+      end
+
+      variant_to_update["product_id"] = @product_id
+      variant_to_update["updated_at"] = Time.now
+
+      variant_to_update
     end
+    
 
     # set updated_at attribute as now
-    product_to_update[:updated_at] = Time.now
+    product_to_update["updated_at"] = Time.now
     
     product_to_update
+
+    return {
+      "product" => product_to_update,
+      "variants" => variants_to_update
+    }
   end
 
   def get_params(object)
-    if object.is_a?(Hash)
-      object = Product.new(object)
+    product_params = {}
+
+    if object["product"].is_a?(Hash)
+      object["product"] = Product.new(object["product"])
     end
-    
-    object.slice(
-      :name, 
-      :sku, 
-      :brand, 
-      :description, 
-      :images, 
-      :sizes, 
-      :old_price, 
-      :price, 
-      :installment_quantity, 
-      :installment_value
-    )
+
+    product_params = {
+      **object["product"].slice(
+        :sku,
+        :brand
+      )
+    }
+
+    variants_params = []
+
+    object["variants"].each do |variant|
+      if variant.is_a?(Hash)
+        variant = ProductVariant.new(variant)
+      end
+
+      variant_params = variant.slice(
+        :title,
+        :full_name,
+        :sku,
+        :description,
+        :images,
+        :sizes,
+        :old_price,
+        :price,
+        :installment_quantity,
+        :installment_value
+      )
+
+      variants_params << variant_params
+    end
+
+
+    return {
+      "product" => product_params,
+      "variants" => variants_params
+    }
   end
 end
--- test/services/get_product_update_object_service_spec.rb
+++ test/services/get_product_update_object_service_spec.rb
@@ -4,146 +4,260 @@ RSpec.describe GetProductUpdateObjectService,
 type: :model do
   context "When testing the GetProductUpdateObjectService class" do
     before :each do
+      sizes = [
+        {
+          size: "p",
+          available: true,
+          url: Faker::Internet.url,
+        },
+       
+        {
+          size: "m",
+          available: false,
+          url: Faker::Internet.url,
+        },
+       
+        {
+          size: "g",
+          available: false,
+          url: Faker::Internet.url,
+        },
+       
+        {
+          size: "gg",
+          available: true,
+          url: Faker::Internet.url,
+        },
+       
+        {
+          size: "xg",
+          available: true,
+          url: Faker::Internet.url,
+        }
+      ]
+
+      product_id = 1
+
       @initial_product = Product.new(
-        name: "Product 1",
+        id: product_id,
         sku: "123456",
         brand: "Brand 1",
+        store: 'store 1',
+        store_url: 'https://store_url_1.com',        
+      )
+      
+      @initial_variant = ProductVariant.new(
+        title: "Product 1",
+        full_name: "Product 1 - White",
+        sku: "123456_WHITE",
         description: "Description 1",
         images: ["image1.jpg", "image2.jpg"],
-        sizes: ["S", "M", "L"],
+        sizes: sizes,
+        available: true,
+        url: Faker::Internet.url,
         old_price: 100.00,
         price: 90.00,
         installment_quantity: 5,
         installment_value: 18.00
       )
 
-      @old_attributes = @initial_product.slice(
-        :name, 
+      @initial_product_object = {
+        "product" => @initial_product,
+        "variants" => [@initial_variant],
+      }
+
+      @old_product_attributes = @initial_product.slice(
         :sku, 
-        :brand, 
+        :brand,
+      )
+
+      @old_variant_attributes = @initial_variant.slice(
+        :title, 
+        :full_name, 
+        :sku, 
         :description, 
         :images, 
         :sizes, 
+        :available,
+        :url,
         :old_price, 
         :price, 
         :installment_quantity, 
         :installment_value
       )
 
-      @new_attributes = {
-        name: "Product 2",
+      @old_object_attributes = {
+        "product" => @old_product_attributes,
+        "variants" => [@old_variant_attributes],
+      }
+
+      @new_product = {
         sku: "654321",
+        brand: "Brand 2",
+        store: 'store 2',
+        store_url: 'https://store_url_2.com',
+      }
+
+      @new_variant = {
+        title: "Product 2",
+        full_name: "Product 2 - Black",
+        sku: "654321_BLACK",
         brand: "Brand 2",
         description: "Description 2",
         images: ["image3.jpg", "image4.jpg"],
-        sizes: ["S", "M", "L"],
-        old_price: 100.00,
-        price: 90.00,
-        installment_quantity: 5,
-        installment_value: 18.00
+        sizes: sizes,
+        available: true,
+        url: Faker::Internet.url,
+        old_price: 110.00,
+        price: 99.00,
+        installment_quantity: 10,
+        installment_value: 9.90
+      }
+
+      @new_product_attributes = @new_product.slice(
+        :sku, 
+        :brand,
+      )
+
+      @new_variant_attributes = @new_variant.slice(
+        :title, 
+        :full_name, 
+        :sku, 
+        :description, 
+        :images, 
+        :sizes, 
+        :available,
+        :url,
+        :old_price, 
+        :price, 
+        :installment_quantity, 
+        :installment_value
+      )
+
+      @new_object_attributes = {
+        "product" => @new_product_attributes,
+        "variants" => [@new_variant_attributes],
       }
     end
 
-    it "should return a object equal to new_attributes if it is just a slice of @initial_product" do
-      new_attributes_equal_to_old_attributes = @old_attributes
-      update_object = GetProductUpdateObjectService.new(@initial_product, new_attributes_equal_to_old_attributes).call
-      expect(update_object).to eq(@old_attributes)
+    it "should return a object equal to new_attributes if it is just a slice of @initial_product", focus: true do
+      new_attributes_equal_to_old_attributes = @old_object_attributes
+      update_object = GetProductUpdateObjectService.new(@initial_product_object, new_attributes_equal_to_old_attributes).call
+      debugger
+      expect(update_object).to eq(@old_object_attributes)
     end
 
     it "should return a object equal to new_attributes all its attribues are not nil" do
-      update_object = GetProductUpdateObjectService.new(@initial_product, @new_attributes).call
-      expect(update_object).to eq(@new_attributes)
+      update_object = GetProductUpdateObjectService.new(@initial_product_object, @new_object_attributes).call
+      expect(update_object).to eq(@new_object_attributes)
     end
 
     it "should return old attributes if new name, sku, brand, description, images and sizes attributes are missing" do
-      new_attributes = @new_attributes
-      new_attributes[:name] = nil
-      new_attributes[:sku] = nil
-      new_attributes[:brand] = nil
-      new_attributes[:description] = nil
-      new_attributes[:images] = []
-      new_attributes[:sizes] = []
+      new_attributes = @new__object_attributes
+
+      new_attributes["product"][:brand] = nil
+      new_attributes["product"][:sku] = nil
+
+      new_attributes["variants"][0][:title] = nil
+      new_attributes["variants"][0][:full_name] = nil
+      new_attributes["variants"][0][:sku] = nil
+      new_attributes["variants"][0][:description] = nil
+      new_attributes["variants"][0][:images] = []
+      new_attributes["variants"][0][:sizes] = []
       
-      update_object = GetProductUpdateObjectService.new(@initial_product, new_attributes).call
+      update_object = GetProductUpdateObjectService.new(@initial_product_object, new_attributes).call
+      update_product = update_object["product"]
+      update_variant = update_object["variants"][0]
       
-      expect(update_object[:name]).to eq(@initial_product.name)
-      expect(update_object[:sku]).to eq(@initial_product.sku)
-      expect(update_object[:brand]).to eq(@initial_product.brand)
-      expect(update_object[:description]).to eq(@initial_product.description)
-      expect(update_object[:images]).to eq(@initial_product.images)
-      expect(update_object[:sizes]).to eq(@initial_product.sizes)
+      expect(update_product[:sku]).to eq(@initial_product.sku)
+      expect(update_product[:brand]).to eq(@initial_product.brand)
+
+      expect(update_variant[:title]).to eq(@initial_variant.title)
+      expect(update_variant[:full_name]).to eq(@initial_variant.full_name)
+      expect(update_variant[:sku]).to eq(@initial_variant.sku)
+      expect(update_variant[:description]).to eq(@initial_variant.description)
+      expect(update_variant[:images]).to eq(@initial_variant.images)
+      expect(update_variant[:sizes]).to eq(@initial_variant.sizes)
     end
 
     context "price and old_price update" do
       it "should return new price attributes if both old_price and price attributes are present" do
-        update_object = GetProductUpdateObjectService.new(@initial_product, @new_attributes).call
+        update_object = GetProductUpdateObjectService.new(@initial_product_object, @new_object_attributes).call
+
+        update_variant = update_object["variants"][0]
         
-        expect(update_object[:old_price]).to eq(@new_attributes[:old_price])
-        expect(update_object[:price]).to eq(@new_attributes[:price])
+        expect(update_variant[:old_price]).to eq(@new_variant_attributes[:old_price])
+        expect(update_variant[:price]).to eq(@new_variant_attributes[:price])
       end
 
       it "should return use the old prices attributes if both old_price and price attributes are missing" do
-        new_attributes = @new_attributes
-        new_attributes[:old_price] = nil
-        new_attributes[:price] = nil
+        new_attributes = @new_object_attributes
+        new_attributes["variants"][0][:old_price] = nil
+        new_attributes["variants"][0][:price] = nil
         
-        update_object = GetProductUpdateObjectService.new(@initial_product, new_attributes).call
+        update_object = GetProductUpdateObjectService.new(@initial_product_object, new_attributes).call
+        update_variant = update_object["variants"][0]
         
-        expect(update_object[:old_price]).to eq(@initial_product.old_price)
-        expect(update_object[:price]).to eq(@initial_product.price)
+        expect(update_variant[:old_price]).to eq(@initial_variant.old_price)
+        expect(update_variant[:price]).to eq(@initial_variant.price)
       end
 
       it "should return the new price attributes if only old_price is missing" do
-        new_attributes = @new_attributes
-        new_attributes[:old_price] = nil
+        new_attributes = @new_object_attributes
+        new_attributes["variants"][0][:old_price] = nil
 
-        update_object = GetProductUpdateObjectService.new(@initial_product, new_attributes).call
+        update_object = GetProductUpdateObjectService.new(@initial_product_object, new_attributes).call
+        update_variant = update_object["variants"][0]
 
-        expect(update_object[:old_price]).to eq(nil)
-        expect(update_object[:price]).to eq(new_attributes[:price])
+        expect(update_variant[:old_price]).to eq(nil)
+        expect(update_variant[:price]).to eq(new_attributes["variants"][0][:price])
       end
 
       it "should return the old price attributes only price is missing" do
-        new_attributes = @new_attributes
-        new_attributes[:price] = nil
+        new_attributes = @new_object_attributes
+        new_attributes["variants"][0][:price] = nil
 
-        update_object = GetProductUpdateObjectService.new(@initial_product, new_attributes).call
+        update_object = GetProductUpdateObjectService.new(@initial_product_object, new_attributes).call
+        update_variant = update_object["variants"][0]
 
-        expect(update_object[:old_price]).to eq(@old_attributes[:old_price])
-        expect(update_object[:price]).to eq(@old_attributes[:price])
+        expect(update_variant[:old_price]).to eq(@old_variant_attributes[:old_price])
+        expect(update_variant[:price]).to eq(@old_variant_attributes[:price])
       end
     end
 
     context "installment_quantity and installment_value update" do
       it "should return new installments quantity and value attributes if both are present" do
-        update_object = GetProductUpdateObjectService.new(@initial_product, @new_attributes).call
+        update_object = GetProductUpdateObjectService.new(@initial_product_object, @new_object_attributes).call
+        update_variant = update_object["variants"][0]
 
-        expect(update_object[:installment_quantity]).to eq(@new_attributes[:installment_quantity])
-        expect(update_object[:installment_value]).to eq(@new_attributes[:installment_value])
+        expect(update_variant[:installment_quantity]).to eq(@new_variant_attributes[:installment_quantity])
+        expect(update_variant[:installment_value]).to eq(@new_variant_attributes[:installment_value])
       end
 
       it "should return new installments quantity and value attributes if both are missing AND price is present" do
-        new_attributes = @new_attributes
-        new_attributes[:installment_quantity] = nil
-        new_attributes[:installment_value] = nil
-        new_attributes[:price] = 100.00
+        new_attributes = @new_object_attributes
+        new_attributes["variants"][0][:installment_quantity] = nil
+        new_attributes["variants"][0][:installment_value] = nil
+        new_attributes["variants"][0][:price] = 100.00
 
-        update_object = GetProductUpdateObjectService.new(@initial_product, new_attributes).call
+        update_object = GetProductUpdateObjectService.new(@initial_product_object, new_attributes).call
+        update_variant = update_object["variants"][0]
 
-        expect(update_object[:installment_quantity]).to eq(@new_attributes[:installment_quantity])
-        expect(update_object[:installment_value]).to eq(@new_attributes[:installment_value])
+        expect(update_variant[:installment_quantity]).to eq(@new_variant_attributes[:installment_quantity])
+        expect(update_variant[:installment_value]).to eq(@new_variant_attributes[:installment_value])
       end
 
-      it "should return OLD installments quantity and value attributes if both are missing AND price is NOT present", focus: true do
-        new_attributes = @new_attributes
-        new_attributes[:installment_quantity] = nil
-        new_attributes[:installment_value] = nil
-        new_attributes[:price] = nil
+      it "should return OLD installments quantity and value attributes if both are missing AND price is NOT present" do
+        new_attributes = @new_object_attributes
+        new_attributes["variants"][0][:installment_quantity] = nil
+        new_attributes["variants"][0][:installment_value] = nil
+        new_attributes["variants"][0][:price] = nil
 
-        update_object = GetProductUpdateObjectService.new(@initial_product, new_attributes).call
+        update_object = GetProductUpdateObjectService.new(@initial_product_object, new_attributes).call
+        update_variant = update_object["variants"][0]
 
-        expect(update_object[:installment_quantity]).to eq(@old_attributes[:installment_quantity])
-        expect(update_object[:installment_value]).to eq(@old_attributes[:installment_value])
+        expect(update_variant[:installment_quantity]).to eq(@old_variant_attributes[:installment_quantity])
+        expect(update_variant[:installment_value]).to eq(@old_variant_attributes[:installment_value])
       end
     end
   end
