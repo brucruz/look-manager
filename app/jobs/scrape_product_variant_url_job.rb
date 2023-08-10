113a0d9c9e9592847b2b6b30e94522321d258fcd
Bruno Cruz <bru.cruz@gmail.com>
2023-08-09 22:26:41 -0300

    fix: ScrapeProductVariantUrlJob saving scraped product and its variants correctly

--- app/jobs/scrape_product_variant_url_job.rb
+++ app/jobs/scrape_product_variant_url_job.rb
@@ -12,15 +12,48 @@ class ScrapeProductVariantUrlJob < ApplicationJob
       end
       
       puts "Scraping #{url}"
-      product_variant = Scrapers::ProductScraper.new(url).scrape
-
-      puts "Saving product variant #{product_variant["full_name"]}"
-      @product_variant = ProductVariant.create(product_variant)
-
+      scraped_product, scraped_variants = Scrapers::ProductScraper.new(url).scrape
       puts "Done scraping #{url}"
+
+      ActiveRecord::Base.transaction do
+        puts "Checking if product #{scraped_product["name"]} exists..."
+        product_exists = Product.find_by(
+          sku: scraped_product["sku"],
+          store: scraped_product["store"]
+        )
+
+        puts "Creating product #{scraped_product["name"]}..." if product_exists.nil?
+        product = Product.create(scraped_product) if product_exists.nil?
+
+        puts "Updating product #{scraped_product["name"]}..." if product_exists.present?
+        product = Product.update(product_exists.id, scraped_product) if product_exists.present?
+
+        puts "Finished with product #{scraped_product["name"]}"
+
+        puts "We found #{scraped_variants.count} variants while scraping #{url}"        
+        scraped_variants.each do |variant|
+          puts "Checking if product variant #{variant["full_name"]} exists..."
+          variant_exists = ProductVariant.find_by(
+            sku: variant["sku"],
+            product_id: product.id
+          )
+
+          puts "Creating product variant #{variant["full_name"]}..." if variant_exists.nil?
+          variant = ProductVariant.create(variant) if variant_exists.nil?
+
+          puts "Updating product variant #{variant["full_name"]}..." if variant_exists.present?
+          variant = ProductVariant.update(variant_exists.id, variant) if variant_exists.present?
+
+          puts "Finished with variant #{variant["full_name"]}"
+        end
+      end
+
+      puts "Saved last product #{scraped_product["name"]} and its #{scraped_variants.count} variants"
+
       return @product_variant
     rescue => e
       puts "Error scraping #{url}: #{e.message}"
+      e.backtrace.each { |line| puts line }
     end
   end
 end
